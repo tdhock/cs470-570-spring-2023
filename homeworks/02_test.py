4ea70845380038929992966fe91c9e152c3a2b73
Toby Dylan Hocking <tdhock5@gmail.com>
2023-01-23 09:58:19 -0700

    consistency

--- homeworks/02_test.py
+++ homeworks/02_test.py
@@ -1,6 +1,6 @@
 # put your code for loadBoard, printBoard, possibleMoves here.
 myBoard = loadBoard("board.txt")
 printBoard(myBoard)
-possibleMoves((3,3),myBoard)
-possibleMoves((2,1),myBoard)
+possibleMoves((0,0),myBoard)
+possibleMoves((2,2),myBoard)
 

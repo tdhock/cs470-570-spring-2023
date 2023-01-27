67320c8bd3be3b2d67c114cfcf50ac311aada47b
Toby Dylan Hocking <tdhock5@gmail.com>
2023-01-27 10:03:33 -0700

    slides + demo

--- homeworks/02_demo.py
+++ homeworks/02_demo.py
@@ -8,3 +8,18 @@ my_list
 
 # for printBoard.
 print(" ".join(my_list[0]))
+
+xy_pair = (0,3)
+x, y = xy_pair
+board_size = len(my_list)
+for x_offset in [-1,0,1]:
+    for y_offset in [-1,0,1]:
+        new_pair = (x+x_offset, y+y_offset)
+        is_good = True
+        for coord in new_pair:
+            if coord < board_size and coord >= 0:
+                "good"
+            else:
+                is_good = False
+        if is_good:
+            print(new_pair)

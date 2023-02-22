766177f6f29567a831585d08de41c6db2d61ed72
Toby Dylan Hocking <tdhock5@gmail.com>
2023-02-21 21:15:39 -0700

    revise hw7

--- homeworks/05-07-map/07_tests.py
+++ homeworks/05-07-map/07_tests.py
@@ -1,32 +1,28 @@
-# test 2.2
+# insert your definition of Searcher class here.
+
 x=Searcher("50test.txt", searchType="DEPTH", verbose=False)
 x.setStart('m')
-x.setGoals(['c', 'af', 'ak'])
+x.setGoals('c', 'af', 'ak')
 x.search()
-x.stats()
 
 x=Searcher("50test.txt", searchType="BREADTH", verbose=False)
 x.setStart('m')
-x.setGoals(['c', 'af', 'ak'])
+x.setGoals('c', 'af', 'ak')
 x.search()
-x.stats()
 
 x=Searcher("50test.txt", searchType="BEST", verbose=False)
 x.setStart('m')
-x.setGoals(['c', 'af', 'ak'])
+x.setGoals('c', 'af', 'ak')
 x.search()
-x.stats()
 
 x=Searcher("50test.txt", searchType="A*", verbose=False)
 x.setStart('m')
-x.setGoals(['c', 'af', 'ak'])
+x.setGoals('c', 'af', 'ak')
 x.search()
-x.stats()
 
 x=Searcher("50test.txt", searchType="A*", verbose=False)
 x.setStart('m')
-x.setGoals(['c', 'af'])
+x.setGoals('c', 'af')
 x.search()
-x.myViz.save("50test.png")
-x.stats()
+
 
